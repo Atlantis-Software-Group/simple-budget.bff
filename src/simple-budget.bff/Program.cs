59c149a75ce3ba86e94a4a385646732d8c62d2d8
ShahSwapnil <SwapnilShah99@gmail.com>
2023-12-07 10:23:59 -0500

    Setup Dockerfile and YARP

--- src/simple-budget.bff/Program.cs
+++ src/simple-budget.bff/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddHttpLogging(o => {});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,8 +21,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// app.UseAuthorization();
-
+app.UseAuthorization();
+app.UseHttpLogging();
 app.MapControllers();
-
+app.MapReverseProxy();
 app.Run();
