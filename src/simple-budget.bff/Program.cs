d79936dd55904dcabe73529b65cf7499b4f81b46
ShahSwapnil <SwapnilShah99@gmail.com>
2023-12-19 22:08:51 -0500

    remove blank line

--- src/simple-budget.bff/Program.cs
+++ src/simple-budget.bff/Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.ConfigureServices(builder.Configuration);
 builder.Host.ConfigureHost();
 
-
 var app = builder.Build();
 app.ConfigurePipeline();
 try
