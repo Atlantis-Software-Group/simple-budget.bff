3336ec66c76098340d14063dbe3824cf5c75ac3f
ShahSwapnil <SwapnilShah99@gmail.com>
2023-12-23 13:10:53 -0500

    Update AsgOpenIdConnectEvents.cs
    Update OnTokenValidated method to capture the tokens after they have been fetched. No reason to validate them, since they were fetched directly from IDP.
    The tokens are cached using InMemoryCache. The key is the value of the Sub claim in the access token.
    The value of the Sub claim is stored in a cookie.

--- src/simple-budget.bff/Utilities/AsgOpenIdConnectEvents.cs
+++ src/simple-budget.bff/Utilities/AsgOpenIdConnectEvents.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Extensions.Caching.Memory;
+using simple_budget.bff.Models;
 
 namespace simple_budget.bff.Utilities;
 
@@ -7,6 +9,42 @@ public static class AsgOpenIdConnectEvents
 {
     public static Task OnTokenValidated(TokenValidatedContext ctx)
     {
+        IDictionary<string, string> parameters = ctx.TokenEndpointResponse?.Parameters ?? new Dictionary<string, string>();
+        if ( !parameters.Any() || ctx.Properties is null )
+            return Task.CompletedTask;
+
+        IServiceScopeFactory scopeFactory = ctx.HttpContext.RequestServices.GetRequiredService<IServiceScopeFactory>();
+        using (IServiceScope scope = scopeFactory.CreateScope())
+        {
+            IMemoryCache cache = scope.ServiceProvider.GetRequiredService<IMemoryCache>();
+            MemoryCacheEntryOptions opts = new MemoryCacheEntryOptions()
+                                                .SetAbsoluteExpiration(TimeSpan.FromDays(5))
+                                                .SetPriority(CacheItemPriority.Normal)
+                                                .SetSize(1024);
+            
+            JwtSecurityToken jwt = new JwtSecurityToken(parameters["access_token"]);
+            string cacheKey = jwt.Subject;
+
+            UserAuthenticationInformation userInfo = new UserAuthenticationInformation{
+                AccessToken = parameters["access_token"],
+                IdToken = parameters["id_token"],
+                RefreshToken = parameters["refresh_token"]
+            };
+
+            cache.GetOrCreate(cacheKey, (cacheEntry) => {
+                return userInfo;
+            });
+
+            ctx.Response.Cookies.Append("sub", cacheKey, new CookieOptions{                
+                IsEssential = true,
+                Secure = true,
+                HttpOnly = true,
+                Domain = "localhost",
+                SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Strict,
+                Expires = DateTime.UtcNow.AddSeconds(3500)
+            });
+        }
+
         return Task.CompletedTask;
     }
 }
