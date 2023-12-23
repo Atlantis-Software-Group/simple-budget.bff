31e824b45c64759543bbc019f2f0a45909ffa4e7
ShahSwapnil <SwapnilShah99@gmail.com>
2023-12-23 13:17:28 -0500

    Add UserAuthenticationInformation.cs
    Model represents user's tokens.

---
+++ src/simple-budget.bff/Models/UserAuthenticationInformation.cs
@@ -0,0 +1,8 @@
+﻿namespace simple_budget.bff.Models;
+
+public class UserAuthenticationInformation
+{
+    public string? AccessToken { get; set; }
+    public string? IdToken { get; set; }
+    public string? RefreshToken { get; set; }
+}
