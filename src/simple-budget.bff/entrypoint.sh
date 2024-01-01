62e21f3861a79251174c88d963b3cc3fb662828d
ShahSwapnil <SwapnilShah99@gmail.com>
2023-12-31 22:21:00 -0500

    Update entrypoint.sh
    remove the call to update-ca-certificates. this will be performed by the entrypoint.sh that is built into the container.
    
    Add commands to restore, build and start simple-budget.bff.

--- src/simple-budget.bff/entrypoint.sh
+++ src/simple-budget.bff/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-update-ca-certificates
+cd src
+cd simple-budget.bff
+dotnet restore ./simple-budget.bff.csproj
+dotnet build ./simple-budget.bff.csproj
 dotnet watch ./bin/Debug/net8.0/simple-budget.bff.dll
